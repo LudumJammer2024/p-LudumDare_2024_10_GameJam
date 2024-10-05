33001e8b501d1b4201b0f1600f8247ce704201eb
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-10-05 23:34:59 +0200

    Radar screen shades red after start

--- Assets/Scripts/Managers/GUIManager.cs
+++ Assets/Scripts/Managers/GUIManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GUIManager : Singleton<GUIManager>
@@ -13,5 +11,11 @@ public class GUIManager : Singleton<GUIManager>
             Canvas c = m_diegeticGUIs[i].GetComponent<Canvas>();
             c.worldCamera = Camera.main;
         }
+    }
+
+    public void ShowRadar()
+    {
+        m_diegeticGUIs[0].gameObject.SetActive(false);
+        m_diegeticGUIs[1].gameObject.SetActive(true);
     }
 }
